8836fa3b9cca06d153bcd9fb8793f9f7d6716bbb
mauripsale <maurizio.ipsale@gmail.com>
2025-03-22 15:39:05 +0000

    added flask session to add persistence

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session
 import vertexai
 from vertexai.generative_models import Content, GenerativeModel, Part
 import os
@@ -38,8 +38,17 @@ def gemini_chat():
     else:
         user_input = request.form['user_input']
     logger.log(f"Starting chat session...")
-    chat_model = create_session()
-    logger.log(f"Chat Session created")
+
+        # Check if a chat session exists in the Flask session
+    if 'chat' not in session:
+        logger.log(f"Starting new chat session...")
+        session['chat'] = create_session()  # Store the chat object in the session
+        logger.log(f"New Chat Session created")
+    else:
+        logger.log(f"Reusing existing chat session...")
+
+    chat = session['chat'] # Retrieve the chat object from the session
+
     content = response(chat_model,user_input)
     return jsonify(content=content)
 
